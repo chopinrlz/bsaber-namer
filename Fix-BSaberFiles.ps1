ddc9499f4bfb2ff22af58c928c395bcd5efd870f
Christian Holslin <chopinrlz@gmail.com>
2020-05-04 23:04:01 -0400

    Removed illegal characters from path

--- Fix-BSaberFiles.ps1
+++ Fix-BSaberFiles.ps1
@@ -1,6 +1,27 @@
+function Get-FolderName {
+    [CmdletBinding()]
+    Param(
+        [Parameter(ValueFromPipeline)]
+        $Json
+    )
+    return "$($Json._songName ?? "Song Name") - $($Json._songAuthorName ?? "Author Name") - $($Json._levelAuthorName ?? "Mapper Name")"
+}
+
+function Remove-IllegalCharacters {
+    [CmdletBinding()]
+    Param(
+        [Parameter(ValueFromPipeline)]
+        $Name
+    )
+    [string]$chars = [System.IO.Path]::GetInvalidPathChars()
+    return $Name -replace "[$([Regex]::Escape( $chars ))]"," "
+}
+
 Get-Item "$PSScriptRoot\*" | ? Name -match "[a-f,0-9]{40}" | % {
-    $path = $_.FullName
-    $data = Get-Content "$path\info.dat" | ConvertFrom-Json
-    $name = "$($data._songName ?? "Song Name") - $($data._songAuthorName ?? "Author Name") - $($data._levelAuthorName ?? "Mapper Name")"
-    Rename-Item -Path $destination -NewName $name
+    $newName = Get-Content -LiteralPath ($_.FullName | Join-Path -ChildPath "info.dat") | ConvertFrom-Json | Get-FolderName | Remove-IllegalCharacters
+    if ( -not (Test-Path $newName) ) {
+        Rename-Item -Path $_.FullName -NewName $newName
+    } else {
+        Write-Warning "$newName already exists"
+    }
 }
